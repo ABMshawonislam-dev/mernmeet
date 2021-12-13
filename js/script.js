f2fbc084f3536c0032c8e15c15f49af18520cfff
ABMshawonislam-dev <abmshawonislam@gmail.com>
2021-12-13 10:57:19 +0600

    forgot to push

--- js/script.js
+++ js/script.js
@@ -1,3 +1,68 @@
+let videoProcess = (function(){
+    let peers_connection_ids = []
+    let peers_connection = []
+
+    let serverProcess;
+    function init(SDPfuntion,myid){
+        serverProcess = SDPfuntion
+        myconnectionid = myid
+    }
+
+    let configaration ={
+        connectionServer:[
+            {
+                urls:"stun:stun.l.google.com:19302"
+            },
+            {
+                urls:"stun:stun1.l.google.com:19302"
+            }
+        ]
+    }
+
+    function setConnection(connetid){
+        let connection = new RTCPeerConnection(configaration)
+
+
+        connection.onnegotiationneeded =  async function(event){
+            await setOffer(connetid)
+        }
+
+        connection.onicecandidate = function(event){
+            if(event.candidate){
+                serverProcess(JSON.stringify({iceCandiate: event.candidate}),connetid)
+            }
+        }
+
+        connection.ontrack = function(event){
+
+        }
+
+
+        peers_connection_ids[connetid] = connetid
+        peers_connection[connetid] = connection
+    
+    }
+
+    async function setOffer(connectionID){
+        let connection = peers_connection[connetid]
+        let offer = await connection.createOffer()
+        await connection.setLocalDescription(offer)
+        serverProcess(JSON.stringify({offer:connection.localDescription}),connectionID)
+    }
+
+
+    return {
+        setNewVideoConnection: async function(connetid){
+            await setConnection(connetid)
+        },
+        init: async function (SDPfuntion,myid){
+            await init(SDPfuntion,myid)
+        }
+    }
+})()
+
+
+
 let myvdoapp = (function(){
 
     function init(username,meetingid){
@@ -7,7 +72,14 @@ let myvdoapp = (function(){
     let socket = null
     function userConnectionFromClient(username,meetingid){
         socket = io.connect()
+        function SDPfunction(data,connetionId){
+            socket.emit("SDPprocess",{
+                message: data,
+                connetionId: connetionId
+            })
+        }
         socket.on("connect",()=>{
+            videoProcess.init(SDPfunction,socket.id)
             if(socket.connected){
               if(username != "" && meetingid != "")  {
                   socket.emit("userconnect",{
@@ -20,6 +92,8 @@ let myvdoapp = (function(){
 
         socket.on("myinformation",(dataserver)=>{
             adduservideo(dataserver.myusername,dataserver.connetid)
+            // setNewVideoConnection(dataserver.connetid)
+            videoProcess.setNewVideoConnection(dataserver.connetid)
         })
 
         function adduservideo(myusername,connetid){
